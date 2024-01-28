dcf78c6a0af0b20e19a4405be3beaeddb547c51c
Ahmet Faruk ÖZ <ahmet.oz@metu.edu.tr>
2024-01-28 13:05:08 +0300

    All the necessary files added

---
+++ app.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click",myFunction())
+
+function myFunction() {
+    alert("Sorry, I am not that good of a programmer yet :(")
+}
